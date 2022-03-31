7e50b85726e6ddba928e39605a696e55e4393961
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-03-31 20:17:13 +0300

    Add files via upload

---
+++ XF_LancheApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF_LancheApp.view;
+
+namespace XF_LancheApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new LoginView());
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
