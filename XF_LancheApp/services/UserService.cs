25b1109861416bdda27fa489b67a9ca2e0b76cee
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-03-31 20:21:27 +0300

    Add files via upload

---
+++ XF_LancheApp/services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XF_LancheApp.Model;
+using System.Threading.Tasks;
+using System.Linq;
+using Firebase.Database.Query;
+
+namespace XF_LancheApp.services
+{
+
+
+
+    public class UserService
+    {
+        FirebaseClient client;
+
+        public UserService()
+        {
+            client =
+                new FirebaseClient(" https://my-coach-9875d-default-rtdb.firebaseio.com/ ");
+        }
+
+        public async Task<bool> IsUserExists(string name)
+        {
+            var user = (await client.Child(" Users ")
+                 .OnceAsync<Coaches>())
+                .Where(u => u.Object.Username == name)
+                .FirstOrDefault();
+            return (user != null);
+        }
+
+        public async Task<bool> RegisterUser(string name, string passwd)
+        {
+            if (await IsUserExists(name) == false)
+            {
+                await client.Child(" Coach ")
+                    .PostAsync(new Coaches()
+                    {
+                        Username = name,
+                        Password = passwd
+                    }) ;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public async Task<bool> LoginUser(string name, string passwd)
+        {
+            var user = (await client.Child(" Coach ")
+                .OnceAsync<Coaches>())
+                .Where(u => u.Object.Username == name)
+                .Where(u => u.Object.Password == passwd)
+                .FirstOrDefault();
+            return (user != null);
+        }
+    }
+}
---
+++ XF_LancheApp/viewModel/LoginViewModel.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using XF_LancheApp.services;
+using XF_LancheApp.view;
+
+namespace XF_LancheApp.viewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _Username;
+        public string Username
+        {
+            set
+            {
+                this._Username = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._Username;
+            }
+        }
+        private string _Password;
+        public string Password
+        {
+            set
+            {
+                this._Password = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._Password;
+            }
+        }
+        private bool _Result;
+        public bool Result
+        {
+            set
+            {
+                this._IsBusy = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._IsBusy;
+            }
+        }
+        private bool _IsBusy;
+        public bool IsBusy
+        {
+            set
+            {
+                this._Result = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return this._Result;
+            }
+        }
+        public Command LoginCommand { get; set; }
+        public Command RegisterCommand { get; set; }
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await LoginCommandAsync());
+            RegisterCommand = new Command(async () => await RegisterCommandAsync());
+        }
+        private async Task RegisterCommandAsync()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.RegisterUser(Username, Password);
+                if (Result)
+                    await Application.Current.MainPage.DisplayAlert("Success", "Registered User", "Ok");
+                else
+                    await Application.Current.MainPage.DisplayAlert("Error", "Failed to register user", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        private async Task LoginCommandAsync()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.LoginUser(Username, Password);
+                if (Result)
+                {
+                    Preferences.Set("Username", Username);
+                    await Application.Current.MainPage.Navigation.PushAsync(new HomeView());
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Invalid Username/Password(s)", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
