5decbdfcc07f24077e07fe0446bef47819d2baa9
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-03-31 20:19:54 +0300

    Add files via upload

---
+++ XF_LancheApp/Model/Coaches.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF_LancheApp.Model
+{
+    
+        public class Coaches
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+    }
+
