3fdc1901f38bbbf657710f545a8b2db44559cbfb
TamaraAwaisa <91056871+TamaraAwaisa@users.noreply.github.com>
2022-04-07 21:37:13 +0300

    Add files via upload

---
+++ xamarinFirbase/xamarinFirbase/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Monthly_Calenar.ModelViews;
+
+namespace Monthly_Calenar.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            calendar.MinimumDate = new DateTime(1970, 1, 1);
+            calendar.MaximumDate = new DateTime(2030, 1, 1);
+        }
+    }
+}
