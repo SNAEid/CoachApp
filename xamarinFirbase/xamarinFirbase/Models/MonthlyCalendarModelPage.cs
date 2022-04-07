1f6312343ccf51f147b1d5aae575702e00e23441
TamaraAwaisa <91056871+TamaraAwaisa@users.noreply.github.com>
2022-04-07 21:33:55 +0300

    Add files via upload

---
+++ xamarinFirbase/xamarinFirbase/Models/MonthlyCalendarModelPage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Monthly_Calenar.ModelViews;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Monthly_Calenar.Models
+{
+    public class MonthlyCalendarModelPage : BaseNotifyPropertyChanged
+    {
+        bool _isSelected;
+        string _title, _description;
+        DateTime _date;
+        int _id;
+
+        public ICommand SelectedCommand { get; private set; }
+
+        public MonthlyCalendarModelPage()
+        {
+            SelectedCommand = new Command(() =>
+            {
+                IsSelected = !IsSelected;
+            });
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedImage));
+            }
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ID
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedImage => IsSelected ? "ic_check" : "ic_uncheck";
+    }
+}
