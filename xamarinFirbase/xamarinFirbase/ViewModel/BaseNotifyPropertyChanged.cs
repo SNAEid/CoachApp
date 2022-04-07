355b1c27b0710e5378c0346a6b3e4d657739ae39
Walaa Faqeh <81427224+walaa2211@users.noreply.github.com>
2022-04-07 21:25:17 +0300

    Create BaseNotifyPropertyChanged.cs

---
+++ xamarinFirbase/xamarinFirbase/ViewModel/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LastCa.ModelView
+{
+
+    public class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+
+}
