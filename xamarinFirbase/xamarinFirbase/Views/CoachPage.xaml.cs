4fc98f71e46225541daf88fd5e489a9606e34bb6
SNAEid <100388300+SNAEid@users.noreply.github.com>
2022-04-07 20:56:41 +0300

    Update CoachPage.xaml.cs

--- xamarinFirbase/xamarinFirbase/Views/CoachPage.xaml.cs
+++ xamarinFirbase/xamarinFirbase/Views/CoachPage.xaml.cs
@@ -1,14 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using xamarinFirbase.ModelView;
+using App52.ModelView;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-
-namespace xamarinFirbase.Views
+namespace App52.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoachPage : ContentPage
@@ -18,18 +17,102 @@ namespace xamarinFirbase.Views
             InitializeComponent();
             BindingContext = new CoachesModelView();
         }
-
-       
-
         private async void Onbtn_Clicked(object sender, EventArgs e)
         {
-            
+            await Navigation.PushAsync(new ContinueRegesrtation());
         }
 
-      
     }
 
-   
+    public class PhoneNumberMaskBehavior : Behavior<Entry>
+    {
+        public static PhoneNumberMaskBehavior Instance = new PhoneNumberMaskBehavior();
 
-   
+        ///  
+        /// Attaches when the page is first created.  
+        ///   
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        ///  
+        /// Detaches when the page is destroyed.  
+        ///   
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                // If the new value is longer than the old value, the user is  
+                if (args.OldTextValue != null && args.NewTextValue.Length < args.OldTextValue.Length)
+                    return;
+
+                var value = args.NewTextValue;
+
+                if (value.Length == 3)
+                {
+                    ((Entry)sender).Text += "-";
+                    return;
+                }
+
+                if (value.Length == 7)
+                {
+                    ((Entry)sender).Text += "-";
+                    return;
+                }
+
+                ((Entry)sender).Text = args.NewTextValue;
+            }
+        }
+    }
+
+    public class EntryLengthValidator : Behavior<Entry>
+    {
+        public int MaxLength { get; set; }
+        public int MinLength { get; set; } = 0;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender;
+
+            // if Entry text is longer than valid length  
+            if (entry.Text.Length > this.MaxLength)
+            {
+                string entryText = entry.Text;
+
+                entryText = entryText.Remove(entryText.Length - 1); // remove last char  
+
+                entry.Text = entryText;
+            }
+
+            if (MinLength > 0)
+                if (entry.Text.Length < this.MinLength)
+                {
+                    ((Entry)sender).TextColor = Color.Red;
+                }
+                else
+                    ((Entry)sender).TextColor = Color.Black;
+        }
+    }
 }
