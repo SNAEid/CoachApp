9f0c5f0b1565d6c95a4e46bfc3bbf928b1407ea0
SNAEid <100388300+SNAEid@users.noreply.github.com>
2022-04-07 14:58:30 +0300

    Add files via upload

---
+++ xamarinFirbase/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xamarinFirbase.Views;
+
+namespace xamarinFirbase
+{
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage =
+                new Views.CoachPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
