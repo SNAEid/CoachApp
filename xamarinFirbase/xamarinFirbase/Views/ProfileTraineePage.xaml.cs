72d10646e60e819d6eacdd53e6e13036bb6a1967
Walaa Faqeh <81427224+walaa2211@users.noreply.github.com>
2022-04-14 22:14:28 +0300

    Update ProfileTraineePage.xaml.cs

--- xamarinFirbase/xamarinFirbase/Views/ProfileTraineePage.xaml.cs
+++ xamarinFirbase/xamarinFirbase/Views/ProfileTraineePage.xaml.cs
@@ -1,20 +1,45 @@
-﻿using System;
+using ReadDB.ModelView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace xamarinFirbase.Views
+namespace ReadDB.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProfileTraineePage : ContentPage
+    public partial class CoachProfileView : ContentPage
     {
-        public ProfileTraineePage()
+        public CoachProfileView()
         {
             InitializeComponent();
+            BindingContext = new CoachProfileModelView();
+
+        }
+
+         async void Button_Clicked(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
         }
     }
+
 }
+  
