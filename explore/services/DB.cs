0f9b1a43b3d70470de5bc81953b1c38014735405
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-04-14 21:49:57 +0300

    Add files via upload

---
+++ explore/services/DB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using explore.model;
+using Firebase.Database;
+using Firebase.Database.Query;
+namespace explore.services
+{
+    public class DB
+    {
+        FirebaseClient client;
+
+        public DB()
+        {
+            client = new FirebaseClient("https://my-coach-9875d-default-rtdb.firebaseio.com/");
+
+        }
+        public ObservableCollection<Coaches> getCoach()
+        {
+            var CoachData = client.Child("Coach").AsObservable<Coaches>().AsObservableCollection();
+
+            return CoachData;
+        }
+
+        public async Task AddCoach(string fname, string lname, string  course ,string location ,string phone)
+        {
+            Coaches c = new Coaches() { FistName = fname, LastName = lname,  Course = course , Location=location , Phone=phone};
+            await client.Child("Coach").PostAsync(c);
+        }
+
+
+    }
+}
---
+++ explore/view/explore2.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using explore.modelview;
+using Xamarin.Forms;
+using explore.model;
+using Xamarin.Forms.Xaml;
+
+namespace explore.view
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class explore2 : ContentPage
+    {          
+
+        public explore2()
+        {
+            InitializeComponent();
+            BindingContext = new exploremodelview();
+
+        }
+
+
+
+        private  void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+
+            App.Current.MainPage = new SearchPage();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+            App.Current.MainPage = new Profile();
+
+         
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text == "Cources")
+            {
+                vBox.Color = Color.Silver;
+                pBox.Color = Color.FromHex("#FFA500");
+                listView.IsVisible = false;
+            }
+            else
+            {
+                listView.IsVisible = true;
+
+                
+
+                vBox.Color = Color.FromHex("#FFA500");
+                pBox.Color = Color.Silver;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
