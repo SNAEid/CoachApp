45c5de4ad70f5b24edb49517083fd62829314c97
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-04-20 09:15:04 +0300

    Add files via upload

---
+++ App52/Views/HomePageview.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App52.ModelView;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App52.Services;
+
+namespace App52.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePageview : ContentPage
+    {
+
+
+
+        public HomePageview()
+        {
+            InitializeComponent();
+
+            listBox1.ItemsSource = new HomePageModelView().Coaches;
+            
+
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+            App.Current.MainPage = new Calender();
+
+
+
+
+        }
+
+
+        private void Calender_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new Calender();
+
+
+        }
+
+        private void ButtonHome_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new HomePageview();
+
+
+        }
+
+    }
+}
