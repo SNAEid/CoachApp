f43ac722bc153fb9d9d17236dedbc80463c10ae8
Walaa Faqeh <81427224+walaa2211@users.noreply.github.com>
2022-04-28 12:23:29 +0300

    Add files via upload

---
+++ View/View/WeeklyCal.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Calenders.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.XamarinForms.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calenders.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeeklyCal : ContentPage
+    {
+        public WeeklyCal()
+        {
+            InitializeComponent();
+
+        }
+
+      
+
+        private void buttonViews_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MonthlyCal());
+        }
+    }
+}
